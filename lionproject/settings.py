1686524ef4d0bd7b5a5757d2c6e03a46af48295a
mingsungk <hansov123@naver.com>
2021-08-30 17:39:16 +0900

    edit for deploy

--- lionproject/settings.py
+++ lionproject/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    'SECRET_KEY','django-insecure-f=@f58uoaxx+*(3rj!9+@)k_zaw(f*#o9(*qs9kobm$f=49&wd')
+SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-f=@f58uoaxx+*(3rj!9+@)k_zaw(f*#o9(*qs9kobm$f=49&wd')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG', 'True') != 'False')
